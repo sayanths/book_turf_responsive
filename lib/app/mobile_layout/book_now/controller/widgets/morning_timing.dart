35e3dbad510616b982243fea072be97e874fc27e
sayanth A <sayanthsayu619@gmail.com>
2022-10-20 11:02:12 +0530

    turf timings added:

--- lib/app/mobile_layout/book_now/controller/widgets/morning_timing.dart
+++ lib/app/mobile_layout/book_now/controller/widgets/morning_timing.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:turf_book_second_project/app/mobile_layout/book_now/model/booking_model.dart';
 import 'package:turf_book_second_project/app/utiles/colors.dart';
 
 class MorningTiming extends StatelessWidget {
+  
   const MorningTiming({
     Key? key,
+    
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Wrap(
-      children: [
-        Container(
+      direction: Axis.horizontal,
+      children: List.generate(morningtiming.length, (index) {
+        return  Container(
           margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(5),
             border: Border.all(width: 1, color: grey),
           ),
           padding: const EdgeInsets.all(8),
-          child: const Text(
-            "10:00 AM",
+          child:  Text(
+            morningtiming[index].toString(),
             style: TextStyle(color: red),
           ),
-        ),
-      ],
+        );
+      })
     );
   }
 }
